35582dd54b48c07ffe5b29cff4440dc76570790c
Савелий Вотинцев <votintcev1@mail.ru>
2026-06-19 02:06:37 +0300

    Страница синхронизации разбита на под-контролы
    - Выделены `SyncToolbar`, `SyncLedgerStrip`, `SyncDiffPanel`
    - `SyncView` сведён к тонкому композитору
    - Стили каждого под-контроля держатся в нём самом

---
+++ SpaceSnoop.Wpf/Views/Sync/SyncDiffPanel.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SpaceSnoop.Wpf.Views.Sync;
+
+public partial class SyncDiffPanel : UserControl
+{
+    public SyncDiffPanel()
+    {
+        InitializeComponent();
+    }
+}
---
+++ SpaceSnoop.Wpf/Views/Sync/SyncLedgerStrip.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SpaceSnoop.Wpf.Views.Sync;
+
+public partial class SyncLedgerStrip : UserControl
+{
+    public SyncLedgerStrip()
+    {
+        InitializeComponent();
+    }
+}
