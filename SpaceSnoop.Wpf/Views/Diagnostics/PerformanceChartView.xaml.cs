2e2ce0e84e09ac15a9d8c1466ac7d1c257aca094
Савелий Вотинцев <votintcev1@mail.ru>
2026-08-01 20:59:49 +0300

    KeepShell 0.1.59: жизненный цикл врезки
    - Обновлены пакеты каркаса до 0.1.59
    - Убран code-behind активации графика: активность врезки теперь ведёт каркас

--- SpaceSnoop.Wpf/Views/Diagnostics/PerformanceChartView.xaml.cs
+++ SpaceSnoop.Wpf/Views/Diagnostics/PerformanceChartView.xaml.cs
@@ -5,18 +5,5 @@ public partial class PerformanceChartView : UserControl, IView<PerformanceChartV
     public PerformanceChartView()
     {
         InitializeComponent();
-        IsVisibleChanged += OnIsVisibleChanged;
-        DataContextChanged += OnDataContextChanged;
-    }
-
-    private void OnIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
-    {
-        (DataContext as PerformanceChartViewModel)?.SetActive(IsVisible);
-    }
-
-    private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
-    {
-        (e.OldValue as PerformanceChartViewModel)?.SetActive(false);
-        (e.NewValue as PerformanceChartViewModel)?.SetActive(IsVisible);
     }
 }
