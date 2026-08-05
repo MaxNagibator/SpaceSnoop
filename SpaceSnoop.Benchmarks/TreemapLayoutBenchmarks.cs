e8c642c5da64940eb5e1c6db490df66c03cd9461
Савелий Вотинцев <votintcev1@mail.ru>
2026-08-05 23:30:54 +0300

    Бенчмарки чистых функций
    - Заведён проект SpaceSnoop.Benchmarks на BenchmarkDotNet, включён в решение
    - Замеряется раскладка карты диска TreemapLayout.Squarify на 50, 500 и 5000 плитках
    - Замеряется построчный diff TextDiff.Compute на 1 000 и 10 000 строках при 10 и 200 правках
    - Данные строятся с фиксированным семенем Random, поэтому подавлено правило S2245

---
+++ SpaceSnoop.Benchmarks/TreemapLayoutBenchmarks.cs
@@ -0,0 +1,36 @@
+﻿using BenchmarkDotNet.Attributes;
+using SpaceSnoop.Wpf.Views.Scan;
+
+namespace SpaceSnoop.Benchmarks;
+
+[MemoryDiagnoser]
+public class TreemapLayoutBenchmarks
+{
+    private const int Seed = 20260805;
+    private const double CanvasWidth = 1280;
+    private const double CanvasHeight = 720;
+
+    private double[] _weights = [];
+
+    [Params(50, 500, 5000)]
+    public int Tiles { get; set; }
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        var random = new Random(Seed);
+
+        _weights = new double[Tiles];
+
+        for (var i = 0; i < Tiles; i++)
+        {
+            _weights[i] = Math.Pow(10, random.NextDouble() * 6);
+        }
+    }
+
+    [Benchmark]
+    public LayoutRect[] Squarify()
+    {
+        return TreemapLayout.Squarify(_weights, CanvasWidth, CanvasHeight);
+    }
+}
