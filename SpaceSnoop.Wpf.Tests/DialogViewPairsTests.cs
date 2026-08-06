e5c98d3d759c817cfed3fea9a37609adeb02453f
Савелий Вотинцев <votintcev1@mail.ru>
2026-08-06 19:09:36 +0300

    Представление диалога поиска дубликатов
    - Добавлен DuplicateProgressDialogView: без него ViewLocator показывал в окне имя типа модели
    - Заведён DialogViewPairsTests, требующий представления каждой модели диалога, иначе следующая пропажа снова обнаружится только глазами

---
+++ SpaceSnoop.Wpf.Tests/DialogViewPairsTests.cs
@@ -0,0 +1,21 @@
+﻿using KeepShell.Bootstrap;
+using SpaceSnoop.Wpf.ViewModels.Dialogs;
+
+namespace SpaceSnoop.Wpf.Tests;
+
+public class DialogViewPairsTests
+{
+    public static IEnumerable<Type> DialogViewModels => typeof(ConfirmDialogViewModel).Assembly
+        .GetTypes()
+        .Where(x => !x.IsAbstract && x.Name.EndsWith("DialogViewModel", StringComparison.Ordinal));
+
+    [TestCaseSource(nameof(DialogViewModels))]
+    public void Каждой_модели_диалога_отвечает_представление(Type model)
+    {
+        var contract = typeof(IView<>).MakeGenericType(model);
+
+        var view = model.Assembly.GetTypes().FirstOrDefault(x => !x.IsAbstract && contract.IsAssignableFrom(x));
+
+        Assert.That(view, Is.Not.Null, $"Для {model.Name} нет представления: окно покажет имя типа вместо диалога.");
+    }
+}
